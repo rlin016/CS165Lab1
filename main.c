c7e05d1e33e5cc3ccc7b787bee130bb8fe0f2267
Raquel Lin <rlin016@ucr.edu>
2021-01-10 22:32:39 -0800

    Wrote down what needs to be done, nothing actually do right now

---
+++ main.c
@@ -0,0 +1,9 @@
+int main(int argc, char* argv[]) {
+	for (/*every possible letter input*/) {
+		//repeat this for loop for every letter since we don't know for sure
+		//after choosing the letter (inside smallest for loop), we call md5 hash from a library
+
+		//NOTE: Check the slide functions to see how the hashed passwords are stored
+		//Potential: breaking the password up into parts ($id$salt$password) for less loops
+	}
+}
