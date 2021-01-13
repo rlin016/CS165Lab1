1f2d4326de789b6b3ce4f444f3ec99240b031ed5
Sana Mohiuddin <smohi002@bolt.cs.ucr.edu>
2021-01-12 21:49:46 -0800

    Fixed most syntax errors

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <string.h>
-#include <md5.h>
+//#include <md5.h>
 
-const base64 = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const char base64[] = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 //taken from slides; change later
-int to64(v, n) {
-  ret = "";
-  for (i = 1; i < n) {
+char* to64(long v, int n) {
+  char* ret = "";  //no actual string in c ig
+
+  for (int i = 1; i < n; i++) {
     ret += base64[v & 0x3f];
     v >>= 6;
   }
@@ -15,4 +16,6 @@ int to64(v, n) {
 
 int main(int argc, char* argv[]) {
 
+
+  return 0;
 }
--- md5.c
+++ md5.c
@@ -1,59 +1,98 @@
-#include <md5.h>
+//#include <md5.h>
+//#include <libcrypto>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 //taking from slides, translating to see what it does; will change to avoid problems LMAO
+
+void 
+substr(char* dest, char* sub, int start, int len)
+{
+
+}
+
+//moved here for convenience lol you can move it back later if you want, I just wanted to look at it all w/o switching files
+const char base64[] = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+char* 
+to64(long v, int n) 
+{
+  char* ret;  //no actual string in c ig
+  for (int i = 1; i < n; i++) {
+    ret += base64[v & 0x3f];
+      v >>= 6;
+  }
+  return ret;
+}
+
 char*
 md5_crypt(const char* pw, const char* salt)
 {
-  res = password + "$1$" + salt;
-  h = MD5(password + salt + password); //maybe use MD5 library here?
-  l = strlen(pw);
-  ("Length of pw: %d", l); //checks to see if strlen works with const char... will write own if not
+  const char* magic = "$1$";
+  // char* res = pw  + magic + salt; //I keep getting an error for using the + for some reason
+  char* res;
+  strcpy(res, pw);
+  strcat(res, magic);
+  strcat(res, salt);
+  // char* h = MD5(pw + salt + pw); //maybe use MD5 library here?
+  char* h;
+  strcpy(h, pw);
+  strcat(h, salt);
+  strcat(h, pw);
+  h = MD5(h);
+  // Why do we add pw twice?
+  //does MD5 return a str? will figure out how to use later...
+  int l = strlen(pw);
+  printf("Length of pw: %d", l); //checks to see if strlen works with const char... will write own if not
 
-  //not sure what this section does...
+  // Replace res with the hashed string of pw + salt + pw ??
+  char* sub;
   while (l > 0) {
-    res = res + substr(h, 0, min(16, l)); //what do i need to use substr?
+    substr(sub, h, 0, fmin(16, l));
+    strcat(res, sub); //what do i need to use substr? // Takes the hashed pw?
     l = l - 16;
   }
-  for (i = strlen(pw); i != 0; i >>= 1) {
+  for (int i = strlen(pw); i != 0; i >>= 1) {
     if (i & 1) {
       res += '\x00'; //no idea what this is for... maybe extra conditions for looping? unknown
     }
     else {
-      res += password[0];
+      res += pw[0];
     }
   }
   h = MD5(res); //second time hashing the new Alternate (is that word LMAO)
 
-  for (i = 0; i < 1000; i++) {
-    tmp = ""; //temp string
+  for (int i = 0; i < 1000; i++) {
+    char *tmp; //temp string
     if (i % 2 == 1) {
-      tmp += password;
+      strcat(tmp, pw);
     }
     else {
-      tmp += h;
+      strcat(tmp, h);
     }
     if (i % 3 != 0) {
-      tmp += salt;
+      strcat(tmp, salt);
     }
     if (i % 7 != 0) {
-      tmp += password;
+      strcat(tmp, pw);
     }
     if (i % 2 == 1) {
-      tmp += h;
+      strcat(tmp, h);
     }
     else {
-      tmp += password;
+      strcat(tmp, pw);
     }
     h = MD5(tmp);
   }
+  char* ret;
+  strcpy(ret, to64((h[0] << 16) | (h[6] << 8) | (h[12]), 4));
+  strcat(ret, to64((h[1] << 16) | (h[7] << 8) | (h[13]), 4));
+  strcat(ret, to64((h[2] << 16) | (h[8] << 8) | (h[14]), 4));
+  strcat(ret, to64((h[3] << 16) | (h[9] << 8) | (h[15]), 4));
+  strcat(ret, to64((h[4] << 16) | (h[10] << 8) | (h[5]), 4));
+  strcat(ret, to64(h[11], 2)); //I DONT KNOW WHAT THIS DOES REALLY; BASE64 THING?
 
-  return to64((h[0] << 16) |
-	      (h[6] << 8) | (h[12]), 4) + to64((h[1] << 16) |
-					       (h[7] << 8) | (h[13]), 4) + to64((h[2] << 16) |
-										(h[8] << 8) | (h[14]), 4) + to64((h[3] << 16) |
-														 (h[9] << 8) | (h[15]), 4) + to64((h[4] << 16) |
-																		  (h[10] << 8) | (h[5]), 4) + to64(h[11], 2); //I DONT KNOW WHAT THIS DOES REALLY; BASE64 THING?
-
+  return ret;
 }
